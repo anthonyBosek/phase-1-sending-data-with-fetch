11e97c9771a57c0125f59ba94562d9fa2f1cbea2
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-14 06:11:48 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+const submitData = (userName, userEmail) => {
+  let userData = {
+    name: userName,
+    email: userEmail,
+  };
+
+  //   need return to pass lab tests - not normally necessary
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((resp) => resp.json())
+    .then((data) => document.body.append(data.id))
+    .catch((err) => document.body.append(`Error: ${err.message}`));
+};
